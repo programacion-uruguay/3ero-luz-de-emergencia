2cd3e00522a47363c7829c3c79a0202af6d0ff95
programacionuy <157076452+programacion-uruguay@users.noreply.github.com>
2024-09-19 08:26:47 -0300

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,7 @@
+basic.forever(function () {
+    if (input.lightLevel() < 20) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+    }
+})
